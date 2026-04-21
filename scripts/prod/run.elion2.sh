e528eb1189fd2528895cec89696bf74922c3778d
Ahmed Said <ahmed.said@jax.org>
2026-04-21 16:53:21 -0400

    now we have a script the theoretically works on elion (#5)

--- scripts/prod/run.elion2.sh
+++ scripts/prod/run.elion2.sh
@@ -1,3 +1,13 @@
 #! /usr/bin/env bash
 
-/sc/service/tools/software/uv/uv run python/update-cellnoor/main.py /sc/service/delivery/*/*/2*
+# Download the binary that downloads Excel sheets
+cd /sc/service/tools/bin
+curl --remote-name --follow https://github.com/demhadais/update-cellnoor/releases/download/download-scbl-excel-workbooks-latest/download-scbl-excel-workbooks
+chmod u+x download-scbl-excel-workbooks
+./download-scbl-excel-workbooks --config_path /sc/service/.config/update-cellnoor/download-scbl-excel-workbooks.toml --output_dir /sc/service/tmp/scbl-excel-workbooks
+
+# Download the python package that sends these updates to the cellnoor API
+cd /sc/service/tmp
+git clone https://github.com/demhadais/update-cellnoor
+cd update-cellnoor/python
+uv run main.py --config-path /sc/service/.config/update-cellnoor/update-cellnoor.toml /sc/service/delivery/*/*/2*
