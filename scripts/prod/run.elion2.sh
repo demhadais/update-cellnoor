c234b90e2b3396e945498a1339c7b8ecfc2f552b
Ahmed Said <ahmed.said@jax.org>
2026-04-21 17:39:25 -0400

    fix the gh workflow to release with a version name. now thats niceee

--- scripts/prod/run.elion2.sh
+++ scripts/prod/run.elion2.sh
@@ -2,7 +2,7 @@
 
 # Download the binary that downloads Excel sheets
 cd /sc/service/tools/bin
-curl --remote-name --follow https://github.com/demhadais/update-cellnoor/releases/download/download-scbl-excel-workbooks-latest/download-scbl-excel-workbooks
+curl --remote-name --follow https://github.com/demhadais/update-cellnoor/releases/latest/download/download-scbl-excel-workbooks
 chmod u+x download-scbl-excel-workbooks
 ./download-scbl-excel-workbooks --config_path /sc/service/.config/update-cellnoor/download-scbl-excel-workbooks.toml --output_dir /sc/service/tmp/scbl-excel-workbooks
 
