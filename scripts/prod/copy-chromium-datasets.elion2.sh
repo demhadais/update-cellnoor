4606c47e96ef4bc3b8a02f3aab7095bf35a0034a
Ahmed Said <ahmed.said@jax.org>
2026-04-22 11:48:51 -0400

    fix script

--- scripts/prod/copy-chromium-datasets.elion2.sh
+++ scripts/prod/copy-chromium-datasets.elion2.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
+set -euo pipefail
+
 cd /sc/service/tmp/update-cellnoor
-rm -rf update-cellnoor
-git clone https://github.com/demhadais/update-cellnoor
 cd update-cellnoor/python/update-cellnoor
 
 uv run copy_chromium_datasets.py ../../../chromium-dataset-copies
