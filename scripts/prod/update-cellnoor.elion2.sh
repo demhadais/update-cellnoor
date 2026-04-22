4f1cd95bda288b9fe345bb0879dc77d23dc45af9
Ahmed Said <ahmed.said@jax.org>
2026-04-22 12:02:33 -0400

    remove the config dir argument

--- scripts/prod/update-cellnoor.elion2.sh
+++ scripts/prod/update-cellnoor.elion2.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 cd /sc/service/tools/bin
 curl --remote-name --follow https://github.com/demhadais/update-cellnoor/releases/latest/download/download-scbl-excel-workbooks
 chmod u+x download-scbl-excel-workbooks
-./download-scbl-excel-workbooks --config_path /sc/service/.config/update-cellnoor/download-scbl-excel-workbooks.toml --output_dir /sc/service/tmp/scbl-excel-workbooks
+./download-scbl-excel-workbooks --output_dir /sc/service/tmp/scbl-excel-workbooks
 
 cd /sc/service/tmp/update-cellnoor/update-cellnoor/python
 
