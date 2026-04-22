ba871442aa01a43dbe271180d249c4a619c9dac4
Ahmed Said <ahmed.said@jax.org>
2026-04-22 12:17:20 -0400

    update sample config and script again

--- scripts/prod/update-cellnoor.elion2.sh
+++ scripts/prod/update-cellnoor.elion2.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-/sc/service/tools/bin/download-scbl-excel-workbooks --output_dir /sc/service/tmp/scbl-excel-workbooks
+/sc/service/tools/bin/download-scbl-excel-workbooks --output_dir /sc/service/tmp/update-cellnoor/scbl-excel-workbooks
 cd /sc/service/tmp/update-cellnoor/update-cellnoor/python/update-cellnoor
 
 uv run main.py ../../../chromium-dataset-copies/*
