01eaa9de0ac1e39c26cb47db7961368ae75be9db
Ahmed Said <ahmed.said@jax.org>
2026-04-22 12:06:31 -0400

    simplify update script too

--- scripts/prod/update-cellnoor.elion2.sh
+++ scripts/prod/update-cellnoor.elion2.sh
@@ -2,12 +2,7 @@
 
 set -euo pipefail
 
-# Download the binary that downloads Excel sheets
-cd /sc/service/tools/bin
-curl --remote-name --follow https://github.com/demhadais/update-cellnoor/releases/latest/download/download-scbl-excel-workbooks
-chmod u+x download-scbl-excel-workbooks
-./download-scbl-excel-workbooks --output_dir /sc/service/tmp/scbl-excel-workbooks
-
+/sc/service/tools/bin/download-scbl-excel-workbooks --output_dir /sc/service/tmp/scbl-excel-workbooks
 cd /sc/service/tmp/update-cellnoor/update-cellnoor/python
 
 uv run main.py ../../../chromium-dataset-copies/*
