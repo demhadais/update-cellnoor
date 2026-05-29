98d2082d062d2b47de9e672f1c4aa82fdd8e59f2
Ahmed Said <ahmed.said@jax.org>
2026-05-29 10:51:04 -0400

    fix prod scripts to correctly load modules

--- scripts/prod/update-cellnoor.elion2.sh
+++ scripts/prod/update-cellnoor.elion2.sh
@@ -5,4 +5,5 @@ set -euo pipefail
 /sc/service/tools/bin/download-scbl-excel-workbooks --output_dir /sc/service/tmp/update-cellnoor/scbl-excel-workbooks
 cd /sc/service/tmp/update-cellnoor/update-cellnoor/python/update-cellnoor
 
+module unload conda && module load /sc/service/tools/modules/languages/uv
 uv run main.py ../../../chromium-dataset-copies/*
