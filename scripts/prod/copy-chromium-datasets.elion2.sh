c5588cf952a01c4c55f8bfbcbcf92443481ac806
Ahmed Said <ahmed.said@jax.org>
2026-05-29 10:59:26 -0400

    fix/module loading (#17)
    * fix prod scripts to correctly load modules
    
    * deactivate specimen measurements because i hate it

--- scripts/prod/copy-chromium-datasets.elion2.sh
+++ scripts/prod/copy-chromium-datasets.elion2.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 cd /sc/service/tmp/update-cellnoor
 cd update-cellnoor/python/update-cellnoor
 
-module unload conda
-module load uv
+module unload conda && module load /sc/service/tools/modules/languages/uv
 
 uv run copy_chromium_datasets.py ../../../chromium-dataset-copies
