56c295474ab430bde789f76b731d4fe3850cdfb3
Ahmed Said <ahmed.said@jax.org>
2026-06-17 13:47:23 -0400

    fix prod scripts

--- scripts/prod/update-cellnoor.elion2.sh
+++ scripts/prod/update-cellnoor.elion2.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-/sc/service/tools/bin/download-scbl-excel-workbooks --output_dir /sc/service/tmp/update-cellnoor/scbl-excel-workbooks
+/sc/service/tools/bin/download-scbl-excel-workbooks --config_path /sc/service/.config/update-cellnoor/download-scbl-excel-workbooks.toml --output_dir /sc/service/tmp/update-cellnoor/scbl-excel-workbooks
 cd /sc/service/tmp/update-cellnoor/update-cellnoor/python/update-cellnoor
 
 module unload conda && module load /sc/service/tools/modules/languages/uv
-uv run main.py ../../../chromium-dataset-copies/*
+uv run main.py --config_path /sc/service/.config/update-cellnoor/update-cellnoor.toml ../../../chromium-dataset-copies/*
