6dc9a7dcff2242488cabf981ad7f14a04521f7c2
Ahmed Said <ahmed.said@jax.org>
2026-06-17 14:38:42 -0400

    i love this lang

--- scripts/prod/update-cellnoor.elion2.sh
+++ scripts/prod/update-cellnoor.elion2.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 /sc/service/tools/bin/download-scbl-excel-workbooks --config_path /sc/service/.config/update-cellnoor/download-scbl-excel-workbooks.toml --output_dir /sc/service/tmp/update-cellnoor/scbl-excel-workbooks
-cd /sc/service/tmp/update-cellnoor/update-cellnoor/python/update-cellnoor
 
+cd /sc/service/tmp/update-cellnoor/update-cellnoor/python/update-cellnoor
 module unload conda && module load /sc/service/tools/modules/languages/uv
-uv run main.py --config-path /sc/service/.config/update-cellnoor/update-cellnoor.toml /sc/service/delivery/*/10x-genomics /sc/service/delivery/*/chromium
+uv run main.py --config-path /sc/service/.config/update-cellnoor/update-cellnoor.toml /sc/service/delivery/*/10x-genomics/* /sc/service/delivery/*/chromium/*
