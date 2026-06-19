7dd9c64092048a7c88f662edcb7fb293fe9a88a4
Ahmed Said <ahmed.said@jax.org>
2026-06-19 12:44:47 -0400

    fix account and gem well loading

--- python/update-cellnoor/models/people.py
+++ python/update-cellnoor/models/people.py
@@ -88,7 +88,7 @@ async def csv_to_new_people(
         if not (
             p is None
             or (p.get("email") in pre_existing_people)
-            or (p.get("auth_provider_user_id") in pre_existing_people)
+            or (p.get("account", {}).get("microsoft_entra_oid") in pre_existing_people)
         )
     )
 
